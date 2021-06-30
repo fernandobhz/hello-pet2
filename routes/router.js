a04a990c8401e08da7c251ed2a5bbd4b1af4a3b0
netomachado <netomachado72@gmail.com>
2021-06-29 22:07:55 -0300

    atualizando

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('./controller');
+const controller = require('../controller/controller');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
